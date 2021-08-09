3557bd424157b2b2247073457ad100667e9784d7
Jesus Alcocer Tagua <jesalctag@gmail.com>
2021-08-09 17:01:11 +0200

    Create Backup_folders.py

---
+++ Backup_folders.py
@@ -0,0 +1,28 @@
+from distutils.dir_util import copy_tree
+from datetime import datetime
+
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+
+From=["C:/from/your/path/A",
+      'C:/from/your/path/B'
+      ]
+
+To=["C:/to/your/path/A " + today,
+    'C:/to/your/path/B ' + today
+    ]
+
+substring = "Path/to/Ignore"
+
+for i in range(len(From)):
+  if substring in To[i]: #not to overwrite in a certain folder
+      print("Skipping " + From[i] + "destination path to be excluded")      
+      continue
+  copy_tree(From[i], To[i])
+
+
+
+
+
+
